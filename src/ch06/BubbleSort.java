5b734cf2ef0fe3250a65d2c93275b1f37e907fdf
MARI <mari20201101@gmail.com>
2021-11-21 16:10:24 +0900

    Upgrade bubble sort

--- src/ch06/BubbleSort.java
+++ src/ch06/BubbleSort.java
@@ -12,21 +12,52 @@ public class BubbleSort {
     }
 
     static void sort(int[] a){
-        for (int i = 0; i < a.length; i++) { // 이부분. a.length -1 이어도 OK 왜냐면 어짜피 마지막 원소는
-            //비교할 필요가 없기 때문
+        int counter = 0;
+        for (int i = 0; i < a.length; i++) {
             for (int j = a.length-1; j > i; j--) {
+                counter++;
                 if(a[j] < a[j-1]){
                     swap(a , j,j-1);
                 }
             }
         }
+        System.out.println("비교횟수  : " + counter);
+    }
+
+    static void sort2(int[] a){
+        int counter = 0;
+        for (int i = 0; i < a.length; i++) {
+            boolean isExchanged = false;
+            for (int j = a.length-1; j > i; j--) {
+                counter++;
+                if(a[j] < a[j-1]){
+                    swap(a , j,j-1);
+                    isExchanged = true;
+                }
+                if(! isExchanged) break;
+            }
+
+        }
+        System.out.println("비교횟수 : " + counter);
     }
 
     public static void main(String[] args) {
         int[] arr = new Random().ints(10, 0, 100).toArray();
         System.out.println(Arrays.toString(arr));
+        long start = System.nanoTime();
         sort(arr);
+        long end = System.nanoTime();
         System.out.println(Arrays.toString(arr));
+        System.out.println("수행시간 : " + (end-start) + " ns");
+
+        System.out.println("--------------------------------");
+        int[] arr2 = new Random().ints(10, 0, 100).toArray();
+        System.out.println(Arrays.toString(arr2));
+        long start2 = System.nanoTime();
+        sort2(arr2);
+        long end2 = System.nanoTime();
+        System.out.println(Arrays.toString(arr2));
+        System.out.println("수행시간 : " + (end2-start2) + " ns");
     }
 
 }
