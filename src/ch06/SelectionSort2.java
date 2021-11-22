2088ea62c00afdad130ff5531a83c7c54d537c4b
MARI <mari20201101@gmail.com>
2021-11-22 23:39:05 +0900

    Explore selection sort

---
+++ src/ch06/SelectionSort2.java
@@ -0,0 +1,30 @@
+package ch06;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SelectionSort2 {
+
+    static void swap(int[] arr, int idx1, int idx2){
+        int tmp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = tmp;
+    }
+
+    static void sort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0 ; j--) {
+                if(arr[j]<arr[j-1]){
+                    swap(arr, j, j-1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new Random().ints(10, 0, 100).toArray();
+        System.out.println(Arrays.toString(arr));
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
