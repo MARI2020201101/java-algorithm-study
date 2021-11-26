ea13b066ac87ab952b1d9ad78e05b317fef2b631
YOOK SEUNGHYEON <shyuk2@iparking.co.kr>
2021-11-26 18:28:13 +0900

    Upgrade my own intSet

--- src/ch07/IntSet.java
+++ src/ch07/IntSet.java
@@ -68,6 +68,49 @@ public class IntSet {
 
     }
 
+    public void copyTo(IntSet s){
+        int n = s.max < num ? max:num; //복사할 요소 개수가 max를 넘어서면 안된다!
+        for (int i = 0; i < n; i++) {
+            s.set[i] = set[i];
+        }
+        s.num = n;
+    }
+    public void copyFrom(IntSet s){
+        int n = max < s.num? max:s.num;
+        for (int i = 0; i < n; i++) {
+            set[i] = s.set[i];
+        }
+        num = n;
+    }
+
+    public void unionOf(IntSet s1, IntSet s2){
+        copyFrom(s1);
+        for (int i = 0; i < s2.num; i++) {
+            add(s2.set[i]);
+        }
+    }
+
+
+    public boolean equalTo(IntSet s){
+        if(s.num!=num){
+            return false;
+        }
+        for (int i = 0; i < num; i++) {
+            int j;
+            for (j = 0; j < num; j++) {
+                if(set[i]==s.set[j]){
+                    break;
+                }
+            }
+            if(j==num) return false;
+        }
+        return true;
+    }
+
+    public String toString(){
+        return Arrays.toString(set);
+    }
+
     public static void main(String[] args) {
         IntSet intSet = new IntSet(10);
         intSet.add(1);
@@ -87,5 +130,19 @@ public class IntSet {
         intSet.print();
         System.out.println(intSet.contains(1));
         System.out.println(intSet.contains(2));
+        IntSet intSet2 = new IntSet(10);
+        intSet2.copyFrom(intSet);
+        intSet2.print();
+        intSet.equalTo(intSet2);
+        intSet2.add(8);
+        intSet2.add(9);
+        intSet.equalTo(intSet2);
+        intSet2.remove(8);
+        intSet2.remove(9);
+        intSet2.print();
+        intSet.unionOf(intSet,intSet2); //set이라서 add할때 중복은 제거됨
+        intSet.print();
+        System.out.println(intSet.toString());
+
     }
 }
