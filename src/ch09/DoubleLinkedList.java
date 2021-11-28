2b8ac3e5697d38997689adb5d91952dfd32f3500
MARI <mari20201101@gmail.com>
2021-11-28 22:49:34 +0900

    Practice my own double linked list

--- src/ch09/DoubleLinkedList.java
+++ src/ch09/DoubleLinkedList.java
@@ -1,5 +1,7 @@
 package ch09;
 
+import java.util.Comparator;
+
 public class DoubleLinkedList<E> {
 
     private Node<E> head;
@@ -7,6 +9,40 @@ public class DoubleLinkedList<E> {
 
     public DoubleLinkedList(){
         head = curr = new Node<>();
+    }
+
+    public void removeCurrentNode(){
+        if(isEmpty() || head.next == head) return;
+        curr.prev.next = curr.next;
+        curr.next.prev = curr.prev;
+        curr = curr.prev;
+        if(curr==head) curr =curr.next;
+    }
+    public void addLast(E obj){
+        curr = head.prev;
+        add(obj);
+    }
+    public void addFirst(E obj){
+        curr = head;
+        add(obj);
+    }
+    public void add(E obj){
+        Node<E> node = new Node<>(obj,curr,curr.next);
+        curr.next = node;
+        curr.next.prev = node;
+        curr = node;
+    }
+
+    public E search(E obj, Comparator c){
+        Node<E> ptr = head.next;
+        while(ptr!=head){
+            if(c.compare(ptr.data, obj)==0){
+                curr = ptr;
+                return ptr.data;
+            }
+            ptr = ptr.next;
+        }
+        return null;
     }
 
     public boolean isEmpty(){
